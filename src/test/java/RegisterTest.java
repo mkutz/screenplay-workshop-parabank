5fa62b87e95555e5436afda084a138e9f271895b
Michael Kutz <michael.kutz@rewe-digital.com>
2020-04-02 10:40:41 +0200

    Use randomAlphabetic instead of randomUUID

--- src/test/java/RegisterTest.java
+++ src/test/java/RegisterTest.java
@@ -1,7 +1,7 @@
 import org.testng.annotations.Test;
 import pages.RegisterPage;
 
-import static java.util.UUID.randomUUID;
+import static org.apache.commons.lang3.RandomStringUtils.randomAlphabetic;
 import static org.testng.Assert.assertTrue;
 
 public class RegisterTest extends BaseTest {
@@ -13,7 +13,7 @@ public class RegisterTest extends BaseTest {
         registerPage.inputAddress("155 Bovet Rd #600", "San Mateo", "CA", "94402");
         registerPage.inputPhoneNumber("+1 650-680-1000");
         registerPage.inputSsn("036-44-1383");
-        registerPage.inputCredentials(randomUUID().toString().substring(0,11), "Sup3rS3cr3t!");
+        registerPage.inputCredentials(randomAlphabetic(12), "Sup3rS3cr3t!");
         registerPage.submit();
 
         assertTrue(homePage.isLoggedIn());
