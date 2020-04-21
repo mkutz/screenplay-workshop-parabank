0a3eedaa535aab08edf56e0f0ab6cbb8e6bd0eee
Michael Kutz <michael.kutz@rewe-digital.com>
2020-04-21 10:30:12 +0200

    Fix RegisterTest

--- src/test/java/pageobjects/RegisterTest.java
+++ src/test/java/pageobjects/RegisterTest.java
@@ -3,6 +3,7 @@ package pageobjects;
 import org.testng.annotations.Test;
 import pageobject.pages.RegisterPage;
 
+import static org.apache.commons.lang3.RandomStringUtils.randomAlphabetic;
 import static org.testng.Assert.assertTrue;
 
 public class RegisterTest extends BaseTest {
@@ -14,8 +15,7 @@ public class RegisterTest extends BaseTest {
         registerPage.inputAddress("155 Bovet Rd #600", "San Mateo", "CA", "94402");
         registerPage.inputPhoneNumber("+1 650-680-1000");
         registerPage.inputSsn("036-44-1383");
-        //registerPage.inputCredentials(randomAlphabetic(12), "Sup3rS3cr3t!");
-        registerPage.inputCredentials(testUsername, "Sup3rS3cr3t!");
+        registerPage.inputCredentials(randomAlphabetic(12), "Sup3rS3cr3t!");
         registerPage.submit();
 
         assertTrue(homePage.isLoggedIn());
