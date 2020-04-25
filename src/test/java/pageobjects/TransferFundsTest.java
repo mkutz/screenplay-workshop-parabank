7ae7ed5ed71f94497c36dd6bcf5c8508f2353b55
Michael Kutz <michael.kutz@rewe-digital.com>
2020-04-25 14:18:12 +0200

    Refactor TransferFundsTest

--- src/test/java/pageobjects/TransferFundsTest.java
+++ src/test/java/pageobjects/TransferFundsTest.java
@@ -3,6 +3,7 @@ package pageobjects;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import pageobject.pages.AccountsOverviewPage;
+import pageobject.pages.OpenNewAccountPage;
 import pageobject.pages.TransferFundsPage;
 
 import static org.testng.Assert.assertEquals;
@@ -16,20 +17,24 @@ public class TransferFundsTest extends BaseTest {
 
     @Test
     public void transferFunds() {
-        String newAccountId = homePage.clickOpenNewAccountLink().openAccount().getNewAccountId();
+        int fromAccountIndex = 0;
+        int toAccountIndex = 1;
+        int amountInCents = 1000;
+
+        OpenNewAccountPage openNewAccountPage = homePage.clickOpenNewAccountLink();
+        openNewAccountPage.openAccount().getNewAccountId();
+
         AccountsOverviewPage accountsOverviewPage = homePage.clickAccountsOverviewLink();
-        String mainAccountId = accountsOverviewPage.getAccountIdsList().get(0);
-        int originalFromBalance = accountsOverviewPage.getAccountBalanceInCents(mainAccountId);
-        int originalToBalance = accountsOverviewPage.getAccountBalanceInCents(newAccountId);
-        int transferAmountInCents = 1000;
+        int originalFromBalance = accountsOverviewPage.getAccountBalanceInCents(fromAccountIndex);
+        int originalToBalance = accountsOverviewPage.getAccountBalanceInCents(toAccountIndex);
 
         TransferFundsPage transferFundsPage = homePage.clickTransferFundsLink();
-        transferFundsPage.transfer(transferAmountInCents, mainAccountId, newAccountId);
+        transferFundsPage.transfer(amountInCents, fromAccountIndex, toAccountIndex);
 
         homePage.clickAccountsOverviewLink();
-        assertEquals(accountsOverviewPage.getAccountBalanceInCents(newAccountId),
-                originalToBalance + transferAmountInCents);
         assertEquals(accountsOverviewPage.getMainAccountBalanceInCents(),
-                originalFromBalance - transferAmountInCents);
+                originalFromBalance - amountInCents);
+        assertEquals(accountsOverviewPage.getAccountBalanceInCents(toAccountIndex),
+                originalToBalance + amountInCents);
     }
 }
